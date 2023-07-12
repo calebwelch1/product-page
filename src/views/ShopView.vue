a0ee5d2d1dd5aa6eafef3058bdddabba9355404b
Caleb <calebjohn@posteo.net>
2023-07-12 13:26:48 -0400

    carousel

--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -1,7 +1,7 @@
 
 <template>
   <main>
-    <div
+    <!-- <div
     v-for="item in items.products"
     :key="item.id"
     style="border: 1px solid white;"
@@ -21,12 +21,51 @@
         <p>${{ item.price }}</p>
       </div>
   </div>
+  </div> -->
+  <div class="product-container"
+  >
+    <div
+    class="product-card"
+    v-for="item in items.products"
+    :key="item.id"
+    >
+      <!-- <VueSlickCarousel
+      v-if="item.images.length > 0"
+      :dots="true"
+      v-for="image in item.images"
+      :key="image.id">
+      <img
+      :src="image"
+      alt="Product 1"
+      style="max-height: 14rem; max-width: 18rem;"
+      >
+    </VueSlickCarousel> -->
+    <Carousel :wrap-around="false">
+    <Slide v-for="image in item.images" :key="image">
+      <img
+      :src="image"
+      alt="Product 1"
+      style="max-height: 14rem; max-width: 18rem;"
+      >
+    </Slide>
+
+    <template #addons>
+      <Navigation />
+      <Pagination />
+    </template>
+  </Carousel>
+      <h3>{{ item.title }}</h3>
+      <p>{{item.description}}</p>
+    </div>
   </div>
   </main>
 </template>
 <script>
 import { onMounted } from 'vue';
 import axios from 'axios'
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+
 // { "id": 1, "title": "iPhone 9", "description": "An apple mobile which is nothing like apple", 
 // "price": 549, "discountPercentage": 12.96, "rating": 4.69, "stock": 94, "brand": "Apple",
 // "category": "smartphones", "thumbnail": "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
@@ -34,10 +73,13 @@ import axios from 'axios'
 // const MyCard = app.component(Card);
 
 export default {
-  // components: {
-  //   // shorthand
-  //   Card,
-  // },
+  components: {
+    // shorthand
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
+  },
   data() {
     return {
       items: [],
@@ -47,6 +89,7 @@ export default {
     try {
       const res = await axios.get(`https://dummyjson.com/products`);
       this.items = res.data;
+      console.log(this.items);
     } catch (error) {
       console.log(error);
     }
@@ -87,4 +130,67 @@ body{
   justify-content: space-between;
   }
 }
+
+* {
+      box-sizing: border-box;
+}
+    
+    body {
+      font-family: Arial, sans-serif;
+      margin: 0;
+      padding: 20px;
+    }
+    
+    p {
+      color: black;
+    }
+    
+    .product-container {
+      display: flex;
+  flex-wrap: wrap;
+    }
+    
+    .product-card {
+      border: 1px solid #ddd;
+      border-radius: 8px;
+      padding: 20px;
+      margin: 4px;
+      background-color: #fff;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+      width: 30vw;
+    }
+    
+    .product-card img {
+      width: 100%;
+      height: auto;
+      margin-bottom: 10px;
+    }
+    
+    .product-card h3 {
+      font-size: 18px;
+      margin: 0;
+      margin-bottom: 10px;
+    }
+    
+    .product-card p {
+      font-size: 14px;
+      margin: 0;
+    }
+    @media (max-width: 1490px) {
+      .product-container {
+      display: flex;
+      flex-direction: column;
+      flex-wrap: wrap;
+      }
+      .product-card {
+      border: 1px solid #ddd;
+      border-radius: 8px;
+      padding: 2px;
+      margin-bottom: 25px;
+      margin: 4px;
+      background-color: #fff;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+      width: 70vw;
+    }
+    }
 </style>
